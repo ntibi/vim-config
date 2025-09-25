5ebb2d01083154dd2589a514de56a4ac938478b9
ntibi <ntibi@protonmail.com>
2025-09-25 14:18:55 +0200

    vimtex

--- init.lua
+++ init.lua
@@ -88,6 +88,15 @@ plugins = {
 	'hrsh7th/cmp-path',
 	'hrsh7th/cmp-cmdline',
 	'hrsh7th/nvim-cmp',
+	{
+		"lervag/vimtex",
+		lazy = false, -- we don't want to lazy load VimTeX
+		-- tag = "v2.15", -- uncomment to pin to a specific release
+		init = function()
+			-- VimTeX configuration goes here, e.g.
+			vim.g.vimtex_view_method = "zathura"
+		end
+	}
 }
 
 require("lazy").setup(plugins, opts)
